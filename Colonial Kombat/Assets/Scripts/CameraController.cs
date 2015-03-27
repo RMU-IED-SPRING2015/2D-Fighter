f85a741b3878b7e5a6bdc34125cdb4c7b3cca499
cbpst226 <cbpst226@mail.rmu.edu>
2015-03-27 14:49:27 -0400

    Unfinished Camera Update
    Added Camera to track player/character movement (unfinished)

---
+++ Colonial Kombat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject TrackPlayer;
+
+	// Use this for initialization
+	void Start () {
+		if (TrackPlayer == null)
+			Debug.LogError ("You need to set the track player object in the Unity Editor.");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = TrackPlayer.transform.position;
+
+		Camera.main.transform.LookAt ( new Vector3 (pos.x, pos.y, 0.0f) );
+			//transform.position.Set ( pos.x, pos.y, Camera.main.transform.position.z );
+	}
+}
---
+++ Colonial Kombat/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Character CharacterRef;
+
+
+	// Use this for initialization
+	void Start () {
+		if (CharacterRef == null)
+			Debug.LogError ("You need to set the character in the Unity Editor.");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (CharacterRef != null) {
+			CharacterRef.WalkTo( CharacterRef.transform.position.x + Input.GetAxis( "horizontal" ) );
+		}
+	}
+
+
+}
+
