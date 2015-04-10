2c80d9b94d0a7d2c6561a54a934152c707def67b
japst469 <japst469@mail.rmu.edu>
2015-04-10 13:17:30 -0400

    Character Jumping with Raycast
    Updated the the Character.Jump function to utilize a raycast instead of
    a collision detection.

--- Colonial Kombat/Assets/Scripts/Character.cs
+++ Colonial Kombat/Assets/Scripts/Character.cs
@@ -11,11 +11,13 @@ public class Character : MonoBehaviour {
     private float _currentHP;
 
 	private bool _grounded;
+    private float _jumpedAt = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 //        Limbs = new Limb[Enum.GetValues(typeof(HumanoidLimbs)).Length];
         setupLimbs();
+        _jumpedAt = 0.0f;
 	}
 
     private void setupLimbs()
@@ -37,9 +39,9 @@ public class Character : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    void Update()
+    void fixedUpdate()
     {
-		rigidbody2D.AddForce (-9.81f * Vector2.up);   
+		rigidbody2D.AddForce (Physics2D.gravity);
 	}
 
     public void Injure(float damage)
@@ -71,7 +73,7 @@ public class Character : MonoBehaviour {
     /// <param name="location"></param>
     public void WalkTo(Vector2 location)
     {
-		//float damp = 10.5f;
+		//float damp = 100.5f;
 		//Vector2 temp = new Vector2 (transform.position.x, transform.position.y);
         
 		//rigidbody2D.AddForce( damp*(location -  temp) );
@@ -100,17 +102,27 @@ public class Character : MonoBehaviour {
     public void Jump()
     {
         // Make the character jump.
+        Debug.DrawLine(rigidbody2D.transform.position - 2.3f * rigidbody2D.transform.up, rigidbody2D.transform.position  - 3.0f * rigidbody2D.transform.up);
 
+        RaycastHit2D rhit = Physics2D.Raycast(rigidbody2D.transform.position - 2.3f * rigidbody2D.transform.up, -rigidbody2D.transform.up, 0.3f);
+        if (rhit.collider != null)
+        {
+            Debug.Log(rhit.collider.name);
+            _grounded = true;
+        }
+        else
+            _grounded = false;
 
-
+        Debug.Log(_jumpedAt);
+        Debug.Log(Time.time);
         // Make sure the character has a footing to jump from, and not allow an air jump.
-        if (_grounded) {
+        if ( _grounded && ( Time.time >= _jumpedAt + .15f ) ) {
 			// First update the position of the character.
-			rigidbody2D.AddForce (5000.0f * Vector2.up);
-
+            
+            rigidbody2D.AddForce(11000.0f * rigidbody2D. transform.up);
+            _jumpedAt = Time.time;
 			// Then play the animation.
 		}
-
     }
 
     /// <summary>
@@ -126,41 +138,29 @@ public class Character : MonoBehaviour {
         return footing;
     }
 
-    /// <summary>
-    /// Causes the characters leg limbs to reach forward and try to find a supporting location to move there.
-    /// </summary>
-    private void stepForward()
-    {
-		
-    }
-	public void eat()
-	{
-	}
 
-	/*.void OnTriggerEnter2D( Collider2D col )
+	void OnTriggerEnter2D( Collider2D col )
 	{
-		Debug.Log ("Attacked");
-		GameObject sp = GameObject.Find ("Sparks");
-		sp.particleEmitter.emit = true;
-		sp.transform.position = col.collider2D;
+		
+
 	}
 	void OnTriggerExit2D()
 	{
-		Debug.Log ("Attacked");
-		GameObject.Find ("Sparks").particleEmitter.emit = false;
+		//Debug.Log ("Attacked");
+		//GameObject.Find ("Fireworks").particleEmitter.emit = false;
 	}
-*/
-	void OnCollisionEnter2D( Collision2D col )
+
+	/*.void OnCollisionEnter2D( Collision2D col )
 	{
 		//Debug.Log ("Collision!!");
-		_grounded = true;
+		//_grounded = true;
 
 	}
 
 	void OnCollisionExit2D( Collision2D col )
 	{
-		_grounded = false;
-	}
+		//_grounded = false;
+	}*/
 }
 
 
